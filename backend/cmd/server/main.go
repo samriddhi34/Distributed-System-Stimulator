efcff9893645cc2117980e38250653b6fb68a704
Samriddhi Sinha <samriddhisinha52@gmail.com>
2026-02-10 00:02:05 +0530

    Adding basic backend Go structure

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Distributed System Simulator - Backend Running")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	fmt.Println("server starting on: 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/samriddhi34/backend
+
+go 1.23.6
